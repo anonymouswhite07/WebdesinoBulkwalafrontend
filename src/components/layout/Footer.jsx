5cbe7eeb45f0f6d91678326ac4d979dede104229
anonymouswhite07 <anonymouswhite07@users.noreply.github.com>
2025-12-03 00:41:35 +0530

    Clear all files from repository

--- src/components/layout/Footer.jsx
+++
@@ -1,7 +0,0 @@
-import { FaFacebookF, FaInstagram, FaYoutube } from "react-icons/fa";
-import { Link } from "react-router-dom";
-import FooterResponsive from "./FooterResponsive";
-
-export default function Footer() {
-  return <FooterResponsive />;
-}
--- src/schemas/paymentSchema.js
+++
@@ -1,8 +0,0 @@
-import { z } from "zod";
-
-export const paymentSchema = z.object({
-  amount: z.number().min(1, "Amount must be greater than 0"),
-  paymentMode: z.enum(["online", "netbanking", "cod"], {
-    required_error: "Please select a payment method",
-  }),
-});
